7a527ec71498783833c5ee56a831bbe65dc2d86c
FherAmableRicse <amablericsefher@gmail.com>
2022-05-06 20:49:30 -0500

    update html, css (footer, header e index) , js theme color

--- js/header.js
+++ js/header.js
@@ -1,12 +1,40 @@
 //sfc
 const header = () => {
+
+    const header = document.querySelector('.header');
+    const headerNav = document.querySelector('.header-nav');
+
+    const headerNavThemeIconContainer = document.getElementById('headerNavThemeIconContainer');
+
     const headerNavMenuIcon = document.getElementById('headerNavMenuIcon');
+    const headerNavMenuCloseIcon = document.getElementById('headerNavMenuCloseIcon');
     const headerNavMenuLinkList = document.querySelector('.header-nav__menu-link-list');
+    const headerNavMenuLinkListItems = document.querySelectorAll('.header-nav__menu-link-item');
+
+    const documentScroll = () => {
+        header.classList.toggle('header--scroll', window.scrollY > 0);
+        headerNav.classList.toggle('header-nav--scroll', window.scrollY > 0);
+    };
 
     const toggleMenu = () => {
-        headerNavMenuLinkList.classList.toggle('header-nav__menu-link-list--open');
+        const body = document.querySelector('.body');
+        body.classList.toggle('body--light');
+        headerNavThemeIconContainer.classList.toggle('header-nav__theme-icon-container--active');
     };
-    headerNavMenuIcon.addEventListener('click', toggleMenu);
-}
+    const openMenu = () => {
+        headerNavMenuLinkList.classList.add('header-nav__menu-link-list--open');
+    };
 
+    const closeMenu = () => {
+        headerNavMenuLinkList.classList.remove('header-nav__menu-link-list--open');
+    };
+
+    document.addEventListener('scroll', documentScroll)
+    headerNavThemeIconContainer.addEventListener('click', toggleMenu);
+    headerNavMenuIcon.addEventListener('click', openMenu);
+    headerNavMenuCloseIcon.addEventListener('click', closeMenu);
+    headerNavMenuLinkListItems.forEach((element) => {
+        element.addEventListener('click', closeMenu);
+    });
+}
 export default header;
